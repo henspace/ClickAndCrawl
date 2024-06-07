755b4c513b821d07dead84811994bc9ea1474672
Steve Butler <henspace@users.noreply.github.com>
2024-06-07 09:13:08 +0100

    Fix scaling issue
    closes #13
    No longer auto sync css fonts to canvas sizing and don't adjust vh. Rely on svh when available.

--- src/scripts/constants/autoVersion.js
+++ src/scripts/constants/autoVersion.js
@@ -3,7 +3,7 @@
  * Automatically created version file. Do not edit!
  */
 export const VERSION = {
-  build: '1.0.0-beta+seo0ak',
-  date:'2024-06-06',
+  build: '1.0.0-beta+sep9t9',
+  date:'2024-06-07',
   copyright: '© 2024 Steve Butler'
 }
