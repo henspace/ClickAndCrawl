84e38c4484b5b46638e76cde1ef605979cbb305a
Steve Butler <henspace@users.noreply.github.com>
2024-06-09 18:42:59 +0100

    Updated build.

--- src/scripts/generatedFiles/version.js
+++ src/scripts/generatedFiles/version.js
@@ -3,7 +3,7 @@
  * Automatically created version file. Do not edit!
  */
 export const VERSION = {
-  build: '1.0.1-beta+seto5k',
+  build: '1.0.1-beta+setq8g',
   date:'2024-06-09',
   copyright: '© 2024 Steve Butler'
 }
