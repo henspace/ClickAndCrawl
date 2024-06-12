f480750ca830da646474a548ea569a511bb1d7ef
Steve Butler <henspace@users.noreply.github.com>
2024-06-12 13:55:00 +0100

    Add photo credits and improve letter w in Crawl graphic

--- src/scripts/generatedFiles/version.js
+++ src/scripts/generatedFiles/version.js
@@ -3,7 +3,7 @@
  * Automatically created version file. Do not edit!
  */
 export const VERSION = {
-  build: '1.0.5-beta+sexru9',
-  date:'2024-06-11',
+  build: '1.0.6-beta+seyx32',
+  date:'2024-06-12',
   copyright: '© 2024 Steve Butler'
 }
