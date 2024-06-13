35106dd7873bb147a0570bb0936ff264a3315e72
Steve Butler <henspace@users.noreply.github.com>
2024-06-13 11:33:07 +0100

    Correct typo in credits

--- src/scripts/generatedFiles/version.js
+++ src/scripts/generatedFiles/version.js
@@ -3,7 +3,7 @@
  * Automatically created version file. Do not edit!
  */
 export const VERSION = {
-  build: '1.0.7-beta+sf0jyv',
+  build: '1.0.8-beta+sf0l7j',
   date:'2024-06-13',
   copyright: '© 2024 Steve Butler'
 }
